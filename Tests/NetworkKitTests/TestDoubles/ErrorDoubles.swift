1a99885b6ae77038595c9002bbd8b17f7035cf78
brmaciel <44143089+brmaciel@users.noreply.github.com>
2022-01-15 17:47:48 -0300

    Add error test doubles

---
+++ Tests/NetworkKitTests/TestDoubles/ErrorDoubles.swift
@@ -0,0 +1,14 @@
+//
+//  ErrorDoubles.swift
+//  NetworkKitTests
+//
+//  Created by Bruno Maciel on 1/9/22.
+//
+
+import Foundation
+
+enum ErrorDouble: Error {
+    case dummy
+    case errorStub
+}
+
